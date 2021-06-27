252f871ef4cf36d663fae52c6df13609196041cc
AnuragKumar <anuragkumarrr@gmail.com>
2021-06-27 16:27:05 -0500

    Adding props key, and created Header,Main & Footer component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header(props){
+  console.log(props)
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  )
+}
+
+function Main(props){
+  return(
+    <section>
+      <p> We serve the most amazing food around St.Louis.</p>
+      <ul style={{textAlign:"left"}}>
+      {
+        props.dishes.map((dish)=>(
+        <li key={dish.id}>{dish.title}</li>
+        ))
+      }
+      </ul>
+      </section>
+  )
+}
+
+function Footer(props){
+  return(
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  )
+}
+
+const dishes=[
+  "Butter Chicken",
+  "Chicken Korma",
+  "Palak Panner",
+  "Caulifower curry"
+]
+
+const disheObjects = dishes.map((dish,i)=>({id:i,title:dish}))
+
+console.log(disheObjects)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header name="Anurag Kumar"/>
+    <Main adjective="amazing" dishes={disheObjects}/>
+    <Footer year={new Date().getFullYear()}/>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from "./App"
 
 
 
-ReactDOM.render( React.createElement("h1",{style:{color:"blue"}},"Heyy Everyone"),document.getElementById('root'));
+ReactDOM.render( <App/>,document.getElementById('root'));
 
 
